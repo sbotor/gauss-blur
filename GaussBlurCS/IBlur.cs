82f15956973066abc3f3e574cc89a50be8767cb6
Szymon Botor <botorszym@gmail.com>
2021-10-17 22:25:53 +0200

    Simple GUI work done

---
+++ GaussBlurCS/IBlur.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GaussBlurCS
+{
+    interface IBlur
+    {
+        protected float[] CreateKernel(int size, float sd);
+
+        public byte[] BlurImage(int start, int end);
+    }
+}
