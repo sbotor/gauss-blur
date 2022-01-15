6a43cbefb942e26f329a49004bcc354e4f08ffea
sbotor <botorszym@gmail.com>
2022-01-15 17:29:10 +0100

    Fixed the BlurThread blur Actions..

--- GaussBlur/Threading/AsmThread.cs
+++ GaussBlur/Threading/AsmThread.cs
@@ -6,26 +6,24 @@ namespace GaussBlur.Threading
 {
     unsafe class AsmThread : BlurThread
     {
-        public BaseAsmFactory? Factory { get; protected set; }
-
         protected Action<long, long> blurX;
         protected Action<long, long> blurY;
 
         public AsmThread(BlurTask task, BaseAsmFactory factory, int start, int end)
             : base(task, start, end)
         {
-            blurX = Factory.BlurX;
-            blurY = Factory.BlurY;
+            blurX = factory.BlurX;
+            blurY = factory.BlurY;
         }
 
         protected override void runX()
         {
-            Factory?.BlurX?.Invoke(StartPos, EndPos);
+            blurX.Invoke(StartPos, EndPos);
         }
 
         protected override void runY()
         {
-            Factory?.BlurY?.Invoke(StartPos, EndPos);
+            blurY.Invoke(StartPos, EndPos);
         }
     }
 }
